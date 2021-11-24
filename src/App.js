40aa3a6ce72f0c0ec7e8b06541e6a2efb4d41db6
JackDevAU <jpr178@live.com.au>
2021-11-24 15:30:42 +1000

    🔇 remove a log

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Widget from './lib/components/Widget'
 
 function App() {
-  console.log(window.location)
   return (
     <div className="App">
       <Widget isDarkMode={true} numberOfRules={5} token={process.env.REACT_APP_GITHUB_PAT} location={window.location}></Widget>
