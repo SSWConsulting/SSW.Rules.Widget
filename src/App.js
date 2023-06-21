5a23d955a4da5fb8aa1897aa4a46d16d72c4bacb
ChloeLin <chloelin@ssw.com.au>
2023-06-21 15:00:05 +0800

    Update package.json version

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { RulesWidget } from './lib';
 function App() {
   return (
     <div className="App">
-      {/* <Widget isDarkMode={true} numberOfRules={5} token={process.env.REACT_APP_GITHUB_PAT} location={window.location}></Widget> */}
       <RulesWidget />
     </div>
   );
