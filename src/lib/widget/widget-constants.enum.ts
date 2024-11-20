e01d78a71fbaad225aa5f8226e2b7e7526da0ede
Babanazar Kamyljanov [SSW] <138548013+babakamyljanovssw@users.noreply.github.com>
2024-11-20 14:12:48 +0800

    ♻️ Removed unused props, created enum for constants, fixed latest rules links (#57)
    * removed unused props, created enum for constants, fixed latest rules url links
    
    * fixed see more url
    
    * 2.0.6
    
    * removed font-family because it is set by rules and people website which is "Inter" font

---
+++ src/lib/widget/widget-constants.enum.ts
@@ -0,0 +1,6 @@
+export enum WidgetConstants {
+  SSWUrl = "https://www.ssw.com.au",
+  RulesUrl = "https://www.ssw.com.au/rules",
+  LatestRulesUrl = "https://www.ssw.com.au/rules/latest-rules/?size=50",
+  UserRulesUrl = "https://www.ssw.com.au/rules/user/?author=",
+}
--- src/lib/widget/widget.tsx
+++ src/lib/widget/widget.tsx
@@ -1,18 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
-import Logo from "../../assets/SSWLogo.png";
 import { FaClock } from "react-icons/fa";
 import { formatDistanceStrict } from "date-fns";
+import { WidgetConstants } from "./widget-constants.enum";
+import Logo from "../../assets/SSWLogo.png";
 import "./styles.css";
 
 const historyJsonUrl = import.meta.env.VITE_HISTORY_JSON_URL;
 const commitsJsonUrl = import.meta.env.VITE_COMMITS_JSON_URL;
 
 export interface WidgetProps {
-  rulesUrl?: string;
-  userRulesUrl?: string;
   showLogo?: boolean;
   location?: string;
-  skip?: number;
   numberOfRules?: number;
   author?: string;
 }
@@ -48,15 +46,14 @@ interface UserCommits {
 }
 
 const Widget = ({
-  rulesUrl = "https://www.ssw.com.au/rules",
-  userRulesUrl = "https://www.ssw.com.au/rules/user/?author=",
   showLogo = false,
   location,
-  skip = 0,
   numberOfRules = 10,
   author,
 }: WidgetProps) => {
-  const sswUrl = "https://www.ssw.com.au";
+  const seeMoreUrl = author
+    ? `${WidgetConstants.UserRulesUrl}${author}`
+    : `${WidgetConstants.LatestRulesUrl}`;
 
   function getLastUpdatedTime(lastUpdatedDate: string) {
     return formatDistanceStrict(new Date(lastUpdatedDate), new Date())
@@ -64,7 +61,11 @@ const Widget = ({
       .replace("second", "sec");
   }
 
-  const { data: historyData, isLoading: historyLoading, error: historyError } = useQuery<Rule[]>({
+  const {
+    data: historyData,
+    isLoading: historyLoading,
+    error: historyError,
+  } = useQuery<Rule[]>({
     queryKey: ["history"],
     queryFn: async () => {
       const response = await fetch(historyJsonUrl);
@@ -72,10 +73,14 @@ const Widget = ({
         throw new Error("Failed to fetch history.json");
       }
       return await response.json();
-    }
+    },
   });
 
-  const { data: commitsData, isLoading: commitsLoading, error: commitsError } = useQuery<UserCommits[]>({
+  const {
+    data: commitsData,
+    isLoading: commitsLoading,
+    error: commitsError,
+  } = useQuery<UserCommits[]>({
     enabled: !!author,
     queryKey: ["commits"],
     queryFn: async () => {
@@ -84,7 +89,7 @@ const Widget = ({
         throw new Error("Failed to fetch commits.json");
       }
       return await response.json();
-    }
+    },
   });
 
   let latestRules: Rule[] = [];
@@ -97,48 +102,56 @@ const Widget = ({
       return 0;
     });
 
-    latestRules = latestRules.filter((rule) => 
-      !rule.isArchived &&
-      rule.uri &&
-      rule.file.indexOf("categories") < 0
+    latestRules = latestRules.filter(
+      (rule) =>
+        !rule.isArchived && rule.uri && rule.file.indexOf("categories") < 0
     );
 
-    latestRules = latestRules.slice(skip, numberOfRules);
+    latestRules = latestRules.slice(0, numberOfRules);
   }
 
   // this section is for widget when it is used People's Profile page
   if (author && commitsData && historyData) {
     const fileMap = new Map<string, { commit: Commit; file: FileChanged }>();
     const userCommits = commitsData.find((user) => user.user === author);
-  
+
     if (userCommits) {
       userCommits.commits.forEach((commit) => {
         commit.FilesChanged.forEach((file) => {
           const existingFile = fileMap.get(file.path);
-          if (!existingFile || new Date(commit.CommitTime) > new Date(existingFile.commit.CommitTime)) {
+          if (
+            !existingFile ||
+            new Date(commit.CommitTime) >
+              new Date(existingFile.commit.CommitTime)
+          ) {
             fileMap.set(file.path, { commit, file });
           }
         });
       });
-    
+
       latestRules = Array.from(fileMap.values()).map(({ file, commit }) => ({
         file: file.path,
         title: file.title,
         uri: file.uri,
-        lastUpdated: commit.CommitTime
+        lastUpdated: commit.CommitTime,
       }));
 
-      latestRules = latestRules.filter((rule) => 
-        historyData.find((historyRule) => historyRule.file === rule.file && !historyRule.isArchived)
+      latestRules = latestRules.filter((rule) =>
+        historyData.find(
+          (historyRule) =>
+            historyRule.file === rule.file && !historyRule.isArchived
+        )
       );
 
-      latestRules = latestRules.slice(skip, numberOfRules);
+      latestRules = latestRules.slice(0, numberOfRules);
     }
   }
 
   function getContent() {
-    if (historyLoading || commitsLoading) return <p className="rw-title">Loading...</p>;
-    else if (historyError || commitsError) return <p className="rw-title">No Rules Found</p>;
+    if (historyLoading || commitsLoading)
+      return <p className="rw-title">Loading...</p>;
+    else if (historyError || commitsError)
+      return <p className="rw-title">No Rules Found</p>;
     else if (latestRules) {
       return (
         <>
@@ -148,7 +161,7 @@ const Widget = ({
                 key={`${item.file}${idx}`}
                 rel="noreferrer"
                 target="_blank"
-                href={`${rulesUrl}/${item.uri}`}
+                href={`${WidgetConstants.RulesUrl}/${item.uri}`}
               >
                 <div className="rw-rule-card">
                   <p className="rw-rule-title">{item.title}</p>
@@ -162,18 +175,16 @@ const Widget = ({
               </a>
             );
           })}
-          {author ? (
-            <div className="see-more-container">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                className="rw-see-more"
-                href={`${userRulesUrl}${author}`}
-              >
-                See more
-              </a>
-            </div>
-          ) : null}
+          <div className="see-more-container">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              className="rw-see-more"
+              href={`${seeMoreUrl}`}
+            >
+              See more
+            </a>
+          </div>
         </>
       );
     }
@@ -183,17 +194,21 @@ const Widget = ({
     <div className="rw-container">
       <div className="rw-title">
         {showLogo ? (
-          <a href={sswUrl}>
+          <a href={WidgetConstants.SSWUrl}>
             <img src={Logo} alt="SSW Logo" height="60" width="130"></img>
           </a>
         ) : (
           ""
         )}
         <h4>
-          {location === rulesUrl ? (
+          {location === WidgetConstants.RulesUrl ? (
             "Latest Rules"
           ) : (
-            <a rel="noreferrer" target="_blank" href={`${rulesUrl}`}>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href={`${WidgetConstants.LatestRulesUrl}`}
+            >
               Latest Rules
             </a>
           )}
